74e8cf726de8b51c21892ed33f6d3a3eeef7282c
Wu Mingsong <mingsong123@gmail.com>
2019-08-02 16:51:53 +0800

    code cleanup

--- Raman_baseline_correction/baselinecorr/ramanskeletal.py
+++ Raman_baseline_correction/baselinecorr/ramanskeletal.py
@@ -15,26 +15,38 @@ class SuiteRaman():
     def __init__(self):
         print('Hello World!')
 
-    def parsefile(self, filename):
+    def parsefile(self, filename, fliplist, xrng = [0,-1]):
+        self.fliplist = fliplist
         self.filename = filename
         _data = np.loadtxt('{}'.format(self.filename))
         self.x = _data[:, 0]
-        self.x = x[0:]
         self.y = _data[:, 1]
-        self.y = y[0:]
-        self.x = np.array([x]).T
-        self.y = np.array([y]).T
-        self.y = np.asmatrix(y)
-        self.length = len(x)
-        print('Parsing...done.')
 
-    def ramanbaseline(self, polyorder = 3, residual = 0.001, coarseness = 0.0001, window = 50, iterations = 50):
+        self.x = np.array([self.x]).T
+        print(self.x)
+        self.y = np.array([self.y]).T
+        print(self.y)
+
+        if fliplist:
+            self.x = np.flipud(self.x)
+            print(self.x)
+            self.y = np.flipud(self.y)
+            print(self.y)
+
+        self.x = self.x[xrng[0]:xrng[1]]
+        self.y = self.y[xrng[0]:xrng[1]]
+        self.y = np.asmatrix(self.y)
+
+        self.length = len(self.x)
+        print('Parsing...done. Your data length is ', self.length )
+
+    def ramanbaseline(self, polyorder = 3, residual = 0.001, coarseness = 0.0001, window = 50, iterations = 50, savefile = True):
 
         vander = np.ones((self.length,1))
         for i in range(1, polyorder+1):
-            xpower = np.power(x, i)
+            xpower = np.power(self.x, i)
             vander = np.append(vander, xpower, axis=1)
-            print(vander, np.shape(vander))
+            #print(vander, np.shape(vander))
         vander = np.asmatrix(vander)
         print("Vandermonde matrix generated...")
 
@@ -67,23 +79,28 @@ class SuiteRaman():
                 weight = np.diagflat(weight)
                 #print (weight)
             #initialise coefficient vector ((n+1) x 1)
-            co = np.matmul(np.matmul(np.matmul(np.linalg.inv(np.matmul(np.matmul((np.transpose(vander)),weight), vander)), np.transpose(vander)), weight), y)
+            co = np.matmul(np.matmul(np.matmul(np.linalg.inv(np.matmul(np.matmul((np.transpose(vander)),weight), vander)), np.transpose(vander)), weight), self.y)
             #print(co)
             rms = math.sqrt(np.mean(np.power((co - prevco), 2)))
             print('RMS = ', rms, "\n")
-            r = y - np.matmul(vander, co)
+            r = self.y - np.matmul(vander, co)
             d = np.gradient(r, axis=0)
             finalcoarse = rocknroll(d, window)
-            coarsenormalised = (finalcoarse - min(coarse))/(max(coarse)-min(coarse))
+            coarsenormalised = (finalcoarse - min(finalcoarse))/(max(finalcoarse)-min(finalcoarse))
 
             if rms == 0:
                 break
 
         self.residual = np.matmul(vander, co)
-        self.signal = y - self.residual
+        self.signal = self.y - self.residual
 
-        baseddata = np.append(x, signal, axis=1)
-        np.savetxt('BASED_{}.txt'.format(self.filename), baseddata, fmt=['%.2f','%.3f'], delimiter='\t')
+        if savefile:
+            baseddata = np.append(self.x, self.signal, axis=1)
+            np.savetxt('based_{}'.format(self.filename), baseddata, fmt=['%.2f','%.3f'], delimiter='\t')
+        else:
+            pass
+
+        return polyorder
 
     def plotraw(self, xlabel, ylabel):
 
@@ -96,19 +113,26 @@ class SuiteRaman():
         plt.pause(1)
         plt.close()
 
-    def plotbased(self, xlabel, ylabel):
+    #
+    def plotbased(self, xrange: tuple, yrange: tuple, xlabel: str, ylabel: str, freezeplot = True, **kwarg):
 
-        self.ramanbaseline()
+        self.ramanbaseline(**kwarg)
         fig = plt.figure()
+        plt.xlim(xrange)
+        plt.ylim(yrange)
         fig.suptitle('BASED_{}'.format(self.filename), fontsize=15)
         plt.xlabel('{}'.format(xlabel), fontsize=15)
         plt.ylabel('{}'.format(ylabel), fontsize=15)
         plt.plot(self.x, self.y)
         plt.plot(self.x, self.signal)
         plt.plot(self.x, self.residual)
-        plt.show(block=False)
-        plt.pause(1)
-        plt.close()
+
+        if freezeplot:
+            plt.show()
+        else:
+            plt.show(block=False)
+            plt.pause(1)
+            plt.close()
 
 
     def __enter__(self):
