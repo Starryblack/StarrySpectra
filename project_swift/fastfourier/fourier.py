35a2655bdcde320002f0626c66fc1f4542ba4fda
Wu Mingsong <mingsong123@gmail.com>
2020-01-14 03:05:54 +0000

    Initialised SWIFT project software script folder

---
+++ project_swift/fastfourier/fourier.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import math
+import sys
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.odr import *
+from scipy import optimize
+
+from natsort import natsorted
+from sympy.solvers import solve
+from sympy import Symbol
+import time
+import progressbar
+
+class fastFourier():
+
+def __init__(self):
+  pass
+
+
+def __enter__(self):
+  pass
+def __exit__(self, e_type, e_val, traceback):
+  pass
